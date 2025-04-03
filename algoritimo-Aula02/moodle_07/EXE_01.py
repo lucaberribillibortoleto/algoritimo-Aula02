dd8812dd9c305836ae653c392e42690ac1e8f8d0
lucaberribillibortoleto <lucabortoleto@gmail.com>
2025-04-03 18:25:29 -0300

    Aula07

---
+++ algoritimo-Aula02/moodle_07/EXE_01.py
@@ -0,0 +1,8 @@
+l1=int(input("Digite o primeiro lado do triângulo: " ))
+l2=int(input("Digite o segundo lado do triângulo: " ))
+hip = (l1**2) + (l2**2) 
+print(f"Hipotenusa: {hip**0.5: .2f}")
+
+
+         
+         
