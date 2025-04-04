ddcc0f5e5fa259f3cb5cab7fac89926de8fd91e7
lucaberribillibortoleto <lucabortoleto@gmail.com>
2025-04-04 14:57:46 -0300

    Aula08

---
+++ algoritimo-Aula02/Algoritimo-moodle01/Moodle_09/Moodle04.py
@@ -0,0 +1,13 @@
+lista_carros = ['''FUSCA 
+                GOL 
+                VECTRA 
+                ESCORT 
+                PEUGEOT''']
+lista_consumo = ['''5
+                  7
+                 6
+                 11 
+                 2''']
+lista = lista_carros + lista_consumo
+
+print(lista)
