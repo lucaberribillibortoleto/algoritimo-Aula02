edfe21adbf53da51424a56e2c6d8dfc72336a5b6
lucaberribillibortoleto <lucabortoleto@gmail.com>
2025-05-12 14:13:28 -0300

    spotfei

--- algoritimo-Aula02/SpotFEI/SpotFEI.py
+++ algoritimo-Aula02/SpotFEI/SpotFEI.py
@@ -1,13 +1,50 @@
-print("Bem vindo ao SpotFEI!")
-acesso = (input("você deseja se cadastrar 'C' ou fazer login 'L'? "))
-senhas = ['123']
+def carregar_usuarios():
+    usuarios = {}
+    try:
+        with open("usuarios.txt", "r") as file:
+            for linha in file:
+                usuario, senha = linha.strip().split(":")
+                usuarios[usuario] = senha  
+    except FileNotFoundError:
+        pass
+    return usuarios
+
+def salvar_usuarios(usuarios):
+    with open("usuarios.txt", "w") as file:
+        for usuario, senha in usuarios.items():
+            file.write(f"{usuario}:{senha}\n")
+
+usuarios = carregar_usuarios()
+
+
+
+
+
+
+print("Bem-vindo ao SpotFEI!")
+
+acesso = input("Você deseja se cadastrar (C) ou fazer login (L)? ").upper()
 
 if acesso == 'L':
-    senha = (input("Digite sua senha: "))
-    if senha in (senhas):
-        print("usuario existente!")
+    usuario = input("Digite seu usuário: ")
+    senha = input("Digite sua senha: ")
+    
+    if usuario in usuarios and usuarios[usuario] == senha:
+        print("Usuário autenticado com sucesso!")
     else:
-        print("usuario não encontrado!")
+        print("Usuário não encontrado ou senha incorreta!")
+
 elif acesso == 'C':
-    usuario = (input("Digite seu usuario: "))
-    senha = (input("Digite sua senha: "))
+    novo_usuario = input("Digite seu novo usuário: ")
+    
+    if novo_usuario in usuarios:
+        print("Usuário já existe!")
+    else:
+        nova_senha = input("Digite sua nova senha: ")
+        usuarios[novo_usuario] = nova_senha
+        print("Usuário cadastrado com sucesso!")
+        
+        salvar_usuarios(usuarios)
+
+else:
+    print("Opção inválida!")
